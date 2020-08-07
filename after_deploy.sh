a076da3065e5463de32f0a494ee068ef2bff8542
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-08-07 13:12:19 -0700

    update deploy target [skip ci]

--- after_deploy.sh
+++ after_deploy.sh
@@ -3,4 +3,4 @@ git config user.email "EC.Bot@ge.com"
 git config user.name "EC Bot"
 git add k8s/pkg/.
 git commit -m 'helm package update [skip ci]'
-git push https://${GITHUB_TOKEN}@github.com/Enterprise-connect/oci.git HEAD:v1.1beta_helm_pkg
+git push https://${GITHUB_TOKEN}@github.com/EC-Release/oci.git HEAD:v1.1beta_helm_pkg
