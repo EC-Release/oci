e8361b7f988853a664f9c7769ae607583ba0177d
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-08-19 22:32:30 -0700

    Update bootstrap.sh

--- spec/agent/bootstrap.sh
+++ spec/agent/bootstrap.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-source <(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.1beta.linux64.txt)
+#!/bin/sh
+source <$(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.1beta.linux64.txt)
