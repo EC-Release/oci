77465e35e39f51ff5a8cd568cbcde462d9722314
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-09-20 11:15:38 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -38,9 +38,9 @@ printf "\n\n\n*** test v1beta agt mode w/ docker\n\n"
 docker run -it --rm --env-file=k8s/example/gateway.env enterpriseconnect/agent:v1beta > agt.log || cat agt.log
 
 printf "\n\n\n*** test server+tls v1 plugin w/ docker\n\n"
-docker run -it --rm -d --name server+tls --env-file=k8s/example/server+tls.env enterpriseconnect/plugins:v1 && sleep 5 && docker logs server+tls 
+docker run -it --rm -d --name server-tls --env-file=k8s/example/server+tls.env enterpriseconnect/plugins:v1 && sleep 5 && docker logs server-tls 
 printf "\n\n\n*** test client+vln v1 plugin w/ docker\n\n"
-docker run -it --rm -d --name client+vln --env-file=k8s/example/client+vln.env enterpriseconnect/plugins:v1 && sleep 5 && docker logs client+vln
+docker run -it --rm -d --name client-vln --env-file=k8s/example/client+vln.env enterpriseconnect/plugins:v1 && sleep 5 && docker logs client-vln
 
 printf "\n\n\n*** installing minikube for simulating test \n\n"
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.18.1/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
