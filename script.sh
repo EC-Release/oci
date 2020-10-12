1814eb0b42f5e313028cadd7973be8a14c33c0ad
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-10-12 15:38:20 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -59,10 +59,8 @@ eval "$(minikube docker-env --profile=minikube)" && export DOCKER_CLI='docker'
 kubectl create -f k8s/example/default-serviceaccount.yaml
 
 printf "\n\n\n*** install server with tls template in minikube\n\n"
-cat k8s/example/values.yaml
 yq w k8s/example/values.yaml global.agtK8Config.withPlugins.tls.enabled true
 yq w k8s/example/values.yaml global.agtK8Config.withPlugins.vln.enabled false
-cat k8s/example/values.yaml
 helm install k8s/example --debug --set-file global.agtConfig=k8s/example/server+tls.env --generate-name
 printf "\n\n\n*** verify logs in minikube\n\n"
 kubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingresses
@@ -72,9 +70,9 @@ printf "\n\n\n*** done debug go ahead delete all.\n\n"
 kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services && kubectl delete --all ingresses
 
 printf "\n\n\n*** install client with vln multi-contr template in minikube\n\n"
-cat k8s/example/values.yaml | yq w - global.agtK8Config.withPlugins.tls.enabled false | tee k8s/example/values.yaml
-cat k8s/example/values.yaml | yq w - global.agtK8Config.withPlugins.vln.enabled true | tee k8s/example/values.yaml
-cat k8s/example/values.yaml | yq w - global.agtK8Config.withPlugins.vln.remote false | tee k8s/example/values.yaml
+yq w k8s/example/values.yaml global.agtK8Config.withPlugins.tls.enabled false
+yq w k8s/example/values.yaml global.agtK8Config.withPlugins.vln.enabled true
+yq w k8s/example/values.yaml global.agtK8Config.withPlugins.vln.remote false
 helm install k8s/example --debug --set-file global.agtConfig=k8s/example/client+vln.env --generate-name
 printf "\n\n\n*** verify logs in minikube\n\n"
 kubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingresses
