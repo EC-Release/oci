77abec65b4687744614eeac8890dad8fdd1bc972
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-10-12 18:27:45 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -88,7 +88,6 @@ yq w -i k8s/example/values.yaml global.agtK8Config.withPlugins.tls.enabled false
 yq w -i k8s/example/values.yaml global.agtK8Config.withPlugins.vln.enabled true
 yq w -i k8s/example/values.yaml global.agtK8Config.withPlugins.vln.remote false
 helm install k8s/example --debug --set-file global.agtConfig=k8s/example/client+vln.env --generate-name
-cat k8s/example/values.yaml
 printf "\n\n\n*** verify logs in minikube\n\n"
 kubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingresses
 #kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
