6e5c94c1f72017bcd4b0d8976ea95424573d77a5
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-10-15 10:35:44 -0700

    Update k8s-deployment-sim.sh

--- k8s-deployment-sim.sh
+++ k8s-deployment-sim.sh
@@ -15,9 +15,10 @@ yq w -i k8s/example/values.yaml global.agtK8Config.withPlugins.tls.enabled true
 yq w -i k8s/example/values.yaml global.agtK8Config.withPlugins.vln.enabled false
 helm install k8s/example --debug --set-file global.agtConfig=k8s/example/server+tls.env --generate-name
 printf "\n\n\n*** verify logs in minikube\n\n"
-kubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingresses
+ubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingresses
 #kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
-kubectl describe deployments $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
+kubectl rollout status $(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
+#kubectl describe deployments $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
 printf "\n\n\n*** done debug go ahead delete all.\n\n"
 kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services && kubectl delete --all ingresses
 
@@ -29,7 +30,8 @@ helm install k8s/example --debug --set-file global.agtConfig=k8s/example/client+
 printf "\n\n\n*** verify logs in minikube\n\n"
 kubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingresses
 #kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
-kubectl describe deployments $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
+kubectl rollout status $(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
+#kubectl describe deployments $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
 printf "\n\n\n*** done debug go ahead delete all.\n\n"
 kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services && kubectl delete --all ingresses
 
@@ -41,6 +43,7 @@ helm install k8s/example --debug --set-file global.agtConfig=k8s/example/client+
 printf "\n\n\n*** verify logs in minikube\n\n"
 kubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingresses
 #kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
-kubectl describe deployments $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
+kubectl rollout status $(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
+#kubectl describe deployments $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
 printf "\n\n\n*** done debug go ahead delete all.\n\n"
 kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services && kubectl delete --all ingresses
