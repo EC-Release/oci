a2572eff34b53e36969e6436c8cb45a5c27e5e41
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-10-15 09:45:17 -0700

    Create docker-deployment-sim.sh

---
+++ docker-deployment-sim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+printf "\n\n\n*** test v1 agt mode w/ docker\n\n"
+docker run -it --rm --env-file=k8s/example/gateway.env enterpriseconnect/agent:v1 > agt.log || cat agt.log
+printf "\n\n\n*** test v1beta agt mode w/ docker\n\n"
+docker run -it --rm --env-file=k8s/example/gateway.env enterpriseconnect/agent:v1beta > agt.log || cat agt.log
+
+printf "\n\n\n*** test server+tls v1 plugin w/ docker\n\n"
+docker run -it --rm -d --name server-tls --env-file=k8s/example/server+tls.env enterpriseconnect/plugins:v1 && sleep 5 && docker logs server-tls 
+printf "\n\n\n*** test client+vln v1 plugin w/ docker\n\n"
+docker run -it --rm -d --name client-vln --env-file=k8s/example/client+vln.env enterpriseconnect/plugins:v1 && sleep 5 && docker logs client-vln 
