76a9bec0f014d46174ab2a88a60d84f701ad2d23
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-10-15 19:34:37 -0700

    Update k8s-deployment-sim.sh

--- k8s-deployment-sim.sh
+++ k8s-deployment-sim.sh
@@ -4,9 +4,9 @@ curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.18
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.8.1/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 mkdir -p $HOME/.kube $HOME/.minikube
 touch $KUBECONFIG
-sudo minikube start -h --profile=minikube --vm-driver=none --kubernetes-version=v1.18.1
+sudo minikube start --profile=minikube --vm-driver=none --kubernetes-version=v1.18.1
 minikube update-context --profile=minikube
-"sudo chown -R travis: /home/travis/.minikube/"
+sudo chown -R travis: $HOME/.minikube
 eval "$(minikube docker-env --profile=minikube)" && export DOCKER_CLI='docker'
 kubectl create -f k8s/example/default-serviceaccount.yaml
 
