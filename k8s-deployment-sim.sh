3c2edceca587206d1ea9762adcf7d94d3cc14e15
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-10-15 20:30:09 -0700

    Update k8s-deployment-sim.sh

--- k8s-deployment-sim.sh
+++ k8s-deployment-sim.sh
@@ -13,7 +13,7 @@ kubectl create -f k8s/example/default-serviceaccount.yaml
 printf "\n\n\n*** install server with tls template in minikube\n\n"
 yq w -i k8s/example/values.yaml global.agtK8Config.withPlugins.tls.enabled true
 yq w -i k8s/example/values.yaml global.agtK8Config.withPlugins.vln.enabled false
-helm install k8s/example --set-file global.agtConfig=k8s/example/server+tls.env --generate-name
+helm install k8s/example --set-file global.agtConfig=$(pwd)/svc_cred_repo/cf1-server+tls.env --generate-name
 printf "\n\n\n*** verify logs in minikube\n\n"
 kubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingresses
 #kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
