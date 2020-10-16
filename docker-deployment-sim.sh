60f487ef983f963df23565d04f648493d016e82b
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-10-16 12:15:35 -0700

    Update docker-deployment-sim.sh

--- docker-deployment-sim.sh
+++ docker-deployment-sim.sh
@@ -6,6 +6,6 @@ docker run -it --rm --env-file=k8s/example/gateway.env enterpriseconnect/agent:v
 
 printf "\n\n\n*** test server+tls v1 plugin w/ docker\n\n"
 #docker run -it --rm -d --name server-tls --env-file=k8s/example/server+tls.env enterpriseconnect/plugins:v1 && sleep 5 && docker logs server-tls 
-docker run -it --rm -d --name server-tls --env-file=k8s/example/server+tls.env enterpriseconnect/plugins:v1 && sleep 5 && docker logs server-tls 
+docker run -it --rm -d --name server-tls --env-file=k8s/example/server+tls.env enterpriseconnect/plugins:v1 && sleep 10 && docker logs server-tls 
 printf "\n\n\n*** test client+vln v1 plugin w/ docker\n\n"
 docker run -it --rm -d --name client-vln --env-file=k8s/example/client+vln.env enterpriseconnect/plugins:v1 && sleep 5 && docker logs client-vln 
