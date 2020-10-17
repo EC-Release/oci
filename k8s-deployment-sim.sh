92104f8bed2514847a8da7948cc0d3c48846eaa1
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-10-16 23:27:51 -0700

    Update k8s-deployment-sim.sh

--- k8s-deployment-sim.sh
+++ k8s-deployment-sim.sh
@@ -18,10 +18,8 @@ yq w -i k8s/example/values.yaml global.agtK8Config.withPlugins.vln.enabled true
 yq w -i k8s/example/values.yaml global.agtK8Config.withPlugins.vln.remote false
 helm install k8s/example --set-file global.agtConfig=k8s/example/client+vln.env --generate-name
 printf "\n\n\n*** verify logs in minikube\n\n"
-kubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingresses
-#kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
-kubectl rollout status pod/$(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
-#kubectl describe deployments $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
+kubectl get pods
+kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
 printf "\n\n\n*** done debug go ahead delete all.\n\n"
 kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services && kubectl delete --all ingresses
 
