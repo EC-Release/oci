9e9866f06bed4929bc0bf70a3d266dc1f3356d26
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-10-16 22:06:52 -0700

    Update k8s-deployment-sim.sh

--- k8s-deployment-sim.sh
+++ k8s-deployment-sim.sh
@@ -18,12 +18,12 @@ printf "\n\n\n*** verify logs in minikube\n\n"
 kubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingresses
 #kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
 
-sleep 4
+#sleep 4
 #kubectl describe deployments $(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
-kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
-kubectl describe pods $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
+#kubectl describe pods $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
 
 kubectl rollout status deploy/$(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
+kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
 #kubectl describe deployments $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
 printf "\n\n\n*** done debug go ahead delete all.\n\n"
 kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services && kubectl delete --all ingresses
