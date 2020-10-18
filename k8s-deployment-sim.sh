5506a1293f57e294781cc4a5790abb91dc152741
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-10-18 15:51:33 -0700

    Update k8s-deployment-sim.sh

--- k8s-deployment-sim.sh
+++ k8s-deployment-sim.sh
@@ -7,7 +7,7 @@ helm install k8s/example --set-file global.agtConfig=k8s/example/server+tls.env 
 printf "\n\n\n*** verify logs in minikube\n\n"
 kubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingresses
 kubectl rollout status deploy/$(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
-sleep 3
+sleep 2
 kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
 #kubectl describe deployments $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
 printf "\n\n\n*** done debug go ahead delete all.\n\n"
@@ -22,8 +22,9 @@ printf "\n\n\n*** verify logs in minikube\n\n"
 sleep 15
 kubectl describe pods $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
 #kubectl get pods -o=jsonpath='{range .items[*]}{"\n"}{.metadata.name}{":\t"}{range .spec.containers[*]}{.name}{", "}{end}{end}' | sort
-op=$(kubectl get pods -o=jsonpath='{range .items[*]}{range .spec.containers[*]}{.name}{", "}{end}{range .spec.initContainers[*]}{.name}{end}' | sort)
-kubectl logs -p -c $op --since=5m
+pdn=$(kubectl get pods -o=jsonpath='{.items[0].metadata.name}')
+ctns=$(kubectl get pods -o=jsonpath='{range .items[*]}{range .spec.containers[*]}{.name}{", "}{end}{range .spec.initContainers[*]}{.name}{end}' | sort)
+kubectl logs -p $pdn -c $ctns --since=5m
 printf "\n\n\n*** done debug go ahead delete all.\n\n"
 kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services && kubectl delete --all ingresses
 
