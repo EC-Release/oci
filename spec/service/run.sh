b00fd9f5b45cfbe2d1bcc4046b23e64b0938cbe1
ayasuda-ge <70172309+ayasuda-ge@users.noreply.github.com>
2020-10-25 21:16:34 -0700

    Update run.sh

--- spec/service/run.sh
+++ spec/service/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pwd
-ls -la $(pwd) && ls -al $(pwd)/assets
+ls -la $(pwd) && ls -al $(pwd)/assets && ls -al $(pwd)/ec-web-ui && ls -al $(pwd)/lib
 eval "sed -i -e 's#{HOST}#${APP}-${ENV}.${HOST}#g' ./assets/swagger.json"
 eval "sed -i -e 's#{BASE}#${EC_REV}#g' ./assets/swagger.json"
 eval "sed -i -e 's#{ENV}#${ENV}#g' ./assets/swagger.json"
